706e11da442fab9a2ad5a9bd3f35ade935a92fd6
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-11-02 17:23:29 +0200

    lab27f

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,5 +33,90 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent2);
             }
         });
+
+
+        Button task1 = findViewById(R.id.task1);
+        task1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent sentToDetails = new Intent(MainActivity.this, TaskDetailActivity.class);
+                sentToDetails.putExtra("task", "Task One");
+                MainActivity.this.startActivity(sentToDetails);
+            }
+        });
+
+
+        Button task2 = findViewById(R.id.task2);
+        task2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent sentToDetails = new Intent(MainActivity.this, TaskDetailActivity.class);
+                sentToDetails.putExtra("task", "Task Two");
+                MainActivity.this.startActivity(sentToDetails);
+            }
+        });
+
+
+
+
+
+        Button task3 = findViewById(R.id.task3);
+        task3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent sentToDetails = new Intent(MainActivity.this, TaskDetailActivity.class);
+                sentToDetails.putExtra("task", "Task Three");
+                MainActivity.this.startActivity(sentToDetails);
+            }
+
+
+        });
+
+
+
+        Button settings = findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent sentToSettings = new Intent(MainActivity.this, SettingsActivity.class);
+                MainActivity.this.startActivity(sentToSettings);
+            }
+        });
+
+
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        TextView textView = findViewById(R.id.textView1);
+
+
+        textView.setText("WELCOME");
+        textView.setVisibility(View.VISIBLE);
+
+
+
+    }
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        TextView textView = findViewById(R.id.textView1);
+        //TextView settingsupdate = findViewById(R.id.settingsupdated);
+
+        SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String username = p.getString("username", "default");
+
+        textView.setText(username + "'s Task List");
+        textView.setVisibility(View.VISIBLE);
+
+
     }
 }
--- app/src/main/java/com/example/taskmaster/SettingsActivity.java
+++ app/src/main/java/com/example/taskmaster/SettingsActivity.java
@@ -2,7 +2,13 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        TextView userItem = SettingsActivity.this.findViewById(R.id.textview7);
+
+        Button submitUsername = findViewById(R.id.save);
+        submitUsername.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                EditText userInput = findViewById(R.id.username);
+                String userInputText = userInput.getText().toString();
+                userItem.setText(userInputText);
+                SharedPreferences username = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+                SharedPreferences.Editor editor = username.edit();
+                editor.putString("username", userInputText);
+                editor.apply();
+
+
+
+            }
+
+
+
+
+        });
     }
 }

8e0a55d81437e3b0718a88899556df870bbdaa2d
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-11-07 14:32:42 +0200

    lab26

--- app/src/main/java/com/example/taskmaster/AllTaskActivity2.java
+++ app/src/main/java/com/example/taskmaster/AllTaskActivity2.java
@@ -2,7 +2,10 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AllTaskActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class AllTaskActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_task2);
+
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent6 = new Intent(AllTaskActivity2.this,MainActivity.class);
+                startActivity(intent6);
+            }
+        });
     }
 }

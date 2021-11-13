c4c7eb54497fe40827ada8d91028ea09f4adb816
Mohammad N Alkhateeb <82310773+mohnalkhateeb@users.noreply.github.com>
2021-11-13 22:23:39 +0200

    Revert "lab32"

--- README.md
+++ README.md
@@ -51,10 +51,6 @@
 #### All Tasks
 ![allTasks](app/screenshots/newAllTask.png)
 
-# LAB32
-* create a Task resource that replicates our existing Task schema.
-* Update all references to the Task data to instead use AWS Amplify to access your data in DynamoDB instead of in Room
-
 
 
 
--- app/src/main/java/com/example/taskmaster/TasksRecyclerViewAdapter.java
+++ app/src/main/java/com/example/taskmaster/TasksRecyclerViewAdapter.java
@@ -7,19 +7,18 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import com.amplifyframework.datastore.generated.model.TaskToDo;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TasksRecyclerViewAdapter extends RecyclerView.Adapter<TasksRecyclerViewAdapter.TaskViewHolder> {
 
-    List<TaskToDo> allTasks = new ArrayList<TaskToDo>();
+    List<Task> allTasks = new ArrayList<>();
     private OnTaskSelectedListener listener;
     // create a public constructor to be used in the activity (to set the data to the adapter)
 
 
-    public TasksRecyclerViewAdapter(List<TaskToDo> allTasks, OnTaskSelectedListener listener) {
+    public TasksRecyclerViewAdapter(List<Task> allTasks, OnTaskSelectedListener listener) {
         this.allTasks = allTasks;
         this.listener = listener;
     }
@@ -42,7 +41,7 @@ public class TasksRecyclerViewAdapter extends RecyclerView.Adapter<TasksRecycler
 
     @Override
     public void onBindViewHolder(@NonNull TasksRecyclerViewAdapter.TaskViewHolder holder, int position) {
-        TaskToDo taskAtPosition = this.allTasks.get(position);
+        Task taskAtPosition = this.allTasks.get(position);
         // Set the reference of which Task
         holder.mTask = taskAtPosition;
         holder.mTitleView.setText(taskAtPosition.getTitle());
@@ -62,7 +61,7 @@ public class TasksRecyclerViewAdapter extends RecyclerView.Adapter<TasksRecycler
         public final TextView mTitleView;
         public final TextView mBodyView;
         public final TextView mStateView;
-        public TaskToDo mTask;
+        public Task mTask;
 
         public TaskViewHolder(View view) {
             super(view);
@@ -76,6 +75,6 @@ public class TasksRecyclerViewAdapter extends RecyclerView.Adapter<TasksRecycler
 
 
     public interface OnTaskSelectedListener {
-        void onTaskSelected(TaskToDo task);
+        void onTaskSelected(Task task);
     }
 }
